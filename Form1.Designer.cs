b7b9cf3fb5245406a14c3e51521ed92530d9d140
otake01 <hitokuge@gmail.com>
2021-10-21 02:21:27 +0900

    ファイルの追加

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Roulette
+{
+    partial class Roulette
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 1D);
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Roulette));
+            this.ch_roulette = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.la_itemcount = new System.Windows.Forms.Label();
+            this.pa_item = new System.Windows.Forms.Panel();
+            this.la_itemname = new System.Windows.Forms.Label();
+            this.la_ratio = new System.Windows.Forms.Label();
+            this.bu_start = new System.Windows.Forms.Button();
+            this.pi_arrow = new System.Windows.Forms.PictureBox();
+            this.tb_title = new System.Windows.Forms.TextBox();
+            this.la_title = new System.Windows.Forms.Label();
+            this.la_rottime = new System.Windows.Forms.Label();
+            this.num_itemcount = new System.Windows.Forms.NumericUpDown();
+            this.num_rottime = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ch_roulette)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pi_arrow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_itemcount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_rottime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ch_roulette
+            // 
+            this.ch_roulette.BackColor = System.Drawing.SystemColors.Control;
+            chartArea1.Name = "ChartArea1";
+            this.ch_roulette.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.ch_roulette.Legends.Add(legend1);
+            this.ch_roulette.Location = new System.Drawing.Point(30, 30);
+            this.ch_roulette.Name = "ch_roulette";
+            series1.BorderColor = System.Drawing.Color.Gray;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            dataPoint1.IsValueShownAsLabel = true;
+            dataPoint1.IsVisibleInLegend = true;
+            dataPoint1.Label = "a";
+            series1.Points.Add(dataPoint1);
+            this.ch_roulette.Series.Add(series1);
+            this.ch_roulette.Size = new System.Drawing.Size(700, 620);
+            this.ch_roulette.TabIndex = 0;
+            this.ch_roulette.Text = "chart1";
+            title1.Name = "Title1";
+            this.ch_roulette.Titles.Add(title1);
+            this.ch_roulette.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // la_itemcount
+            // 
+            this.la_itemcount.AutoSize = true;
+            this.la_itemcount.Location = new System.Drawing.Point(938, 105);
+            this.la_itemcount.Name = "la_itemcount";
+            this.la_itemcount.Size = new System.Drawing.Size(82, 24);
+            this.la_itemcount.TabIndex = 2;
+            this.la_itemcount.Text = "項目数";
+            this.la_itemcount.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // pa_item
+            // 
+            this.pa_item.AutoScroll = true;
+            this.pa_item.Location = new System.Drawing.Point(750, 214);
+            this.pa_item.Name = "pa_item";
+            this.pa_item.Size = new System.Drawing.Size(300, 299);
+            this.pa_item.TabIndex = 3;
+            // 
+            // la_itemname
+            // 
+            this.la_itemname.AutoSize = true;
+            this.la_itemname.Location = new System.Drawing.Point(796, 181);
+            this.la_itemname.Name = "la_itemname";
+            this.la_itemname.Size = new System.Drawing.Size(82, 24);
+            this.la_itemname.TabIndex = 4;
+            this.la_itemname.Text = "項目名";
+            this.la_itemname.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // la_ratio
+            // 
+            this.la_ratio.AutoSize = true;
+            this.la_ratio.Location = new System.Drawing.Point(956, 181);
+            this.la_ratio.Name = "la_ratio";
+            this.la_ratio.Size = new System.Drawing.Size(58, 24);
+            this.la_ratio.TabIndex = 5;
+            this.la_ratio.Text = "比率";
+            this.la_ratio.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // bu_start
+            // 
+            this.bu_start.Location = new System.Drawing.Point(960, 610);
+            this.bu_start.Name = "bu_start";
+            this.bu_start.Size = new System.Drawing.Size(90, 40);
+            this.bu_start.TabIndex = 0;
+            this.bu_start.Text = "Start!";
+            this.bu_start.UseVisualStyleBackColor = true;
+            this.bu_start.Click += new System.EventHandler(this.bu_start_Click);
+            // 
+            // pi_arrow
+            // 
+            this.pi_arrow.Location = new System.Drawing.Point(96, 76);
+            this.pi_arrow.Name = "pi_arrow";
+            this.pi_arrow.Size = new System.Drawing.Size(512, 512);
+            this.pi_arrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pi_arrow.TabIndex = 6;
+            this.pi_arrow.TabStop = false;
+            this.pi_arrow.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // tb_title
+            // 
+            this.tb_title.Location = new System.Drawing.Point(750, 62);
+            this.tb_title.MaxLength = 30;
+            this.tb_title.Name = "tb_title";
+            this.tb_title.Size = new System.Drawing.Size(270, 31);
+            this.tb_title.TabIndex = 7;
+            this.tb_title.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tb_title.Leave += new System.EventHandler(this.tb_title_Leave);
+            // 
+            // la_title
+            // 
+            this.la_title.AutoSize = true;
+            this.la_title.Location = new System.Drawing.Point(915, 30);
+            this.la_title.Name = "la_title";
+            this.la_title.Size = new System.Drawing.Size(105, 24);
+            this.la_title.TabIndex = 8;
+            this.la_title.Text = "タイトル名";
+            this.la_title.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // la_rottime
+            // 
+            this.la_rottime.AutoSize = true;
+            this.la_rottime.Location = new System.Drawing.Point(826, 528);
+            this.la_rottime.Name = "la_rottime";
+            this.la_rottime.Size = new System.Drawing.Size(194, 24);
+            this.la_rottime.TabIndex = 9;
+            this.la_rottime.Text = "1回転する時間(秒)";
+            this.la_rottime.Click += new System.EventHandler(this.Form_Click);
+            // 
+            // num_itemcount
+            // 
+            this.num_itemcount.Location = new System.Drawing.Point(900, 137);
+            this.num_itemcount.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.num_itemcount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num_itemcount.Name = "num_itemcount";
+            this.num_itemcount.Size = new System.Drawing.Size(120, 31);
+            this.num_itemcount.TabIndex = 10;
+            this.num_itemcount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.num_itemcount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num_itemcount.ValueChanged += new System.EventHandler(this.num_itemcount_ValueChanged);
+            // 
+            // num_rottime
+            // 
+            this.num_rottime.DecimalPlaces = 1;
+            this.num_rottime.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.num_rottime.Location = new System.Drawing.Point(900, 562);
+            this.num_rottime.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.num_rottime.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.num_rottime.Name = "num_rottime";
+            this.num_rottime.Size = new System.Drawing.Size(120, 31);
+            this.num_rottime.TabIndex = 11;
+            this.num_rottime.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.num_rottime.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num_rottime.ValueChanged += new System.EventHandler(this.num_rottime_ValueChanged);
+            // 
+            // Roulette
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 669);
+            this.Controls.Add(this.num_rottime);
+            this.Controls.Add(this.num_itemcount);
+            this.Controls.Add(this.la_rottime);
+            this.Controls.Add(this.la_title);
+            this.Controls.Add(this.tb_title);
+            this.Controls.Add(this.pi_arrow);
+            this.Controls.Add(this.bu_start);
+            this.Controls.Add(this.la_ratio);
+            this.Controls.Add(this.la_itemname);
+            this.Controls.Add(this.pa_item);
+            this.Controls.Add(this.la_itemcount);
+            this.Controls.Add(this.ch_roulette);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Roulette";
+            this.Text = "ドキドキ★ルーレット";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.ch_roulette)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pi_arrow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_itemcount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_rottime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart ch_roulette;
+        private System.Windows.Forms.Label la_itemcount;
+        private System.Windows.Forms.Panel pa_item;
+        private System.Windows.Forms.Label la_itemname;
+        private System.Windows.Forms.Label la_ratio;
+        private System.Windows.Forms.Button bu_start;
+        private System.Windows.Forms.PictureBox pi_arrow;
+        private System.Windows.Forms.TextBox tb_title;
+        private System.Windows.Forms.Label la_title;
+        private System.Windows.Forms.Label la_rottime;
+        private System.Windows.Forms.NumericUpDown num_itemcount;
+        private System.Windows.Forms.NumericUpDown num_rottime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System;
+using System.Media;
+
+namespace Roulette
+{
+    public partial class Roulette : Form
+    {
+        private int datacount = 0; //グラフ入力欄の数
+        private bool rotationflg = false; //回転フラグ(true=回転,false=停止)
+        private int stopcount = 0; //stopボタンを押してからの経過時間
+        private int stoptime = 0;
+
+        private System.Timers.Timer timer;
+        private readonly int ONE_SEC = 1000; // 1秒(ミリ秒)
+        private readonly int MAX_FRAME = 60; // 最大フレーム
+        private readonly int STOP_EXTENSION_MAX_TIME = 30; //ストップ最大延長フレーム
+        private readonly int STOP_EXTENSION_MIN_TIME = 1; //ストップ最小延長フレーム
+
+        private Bitmap org = null;
+        private readonly int YAZIRUSI_PIC_X = 128; //画像サイズ(X方向)
+        private readonly int YAZIRUSI_PIC_Y = 128; //画像サイズ(Y方向)
+
+        private readonly float MAX_ANGLE = 360;
+        private float ang = 0; //現在の角度
+        private float rot_time = 1; //矢印1周にかかる時間(秒)
+
+        private SoundPlayer player = null;
+        private readonly string ROULETTE_SE_PATH = "sozai/roulette.wav"; //回転中に鳴るSEファイルの場所
+
+        private readonly string YAZIRUSI_PATH = "sozai/yazirusi.png"; //矢印画像の場所
+
+        public Roulette()
+        {
+            InitializeComponent();
+        }
+
+        //初期化
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //グラフ初期化
+            ch_roulette.Series.Clear();
+            ch_roulette.ChartAreas.Clear();
+            ch_roulette.Titles.Clear();
+
+            ch_roulette.BackColor = SystemColors.Control;
+
+            string chart_area1 = "Area1";
+            ch_roulette.ChartAreas.Add(new ChartArea(chart_area1));
+            ch_roulette.ChartAreas[chart_area1].BackColor = SystemColors.Control;
+
+            //グラフの種類を円に
+            string legend1 = "Graph1";
+            ch_roulette.Series.Add(legend1);
+            ch_roulette.Series[legend1].ChartType = SeriesChartType.Pie;
+
+            ch_roulette.Series[legend1].BorderColor = Color.Black;
+            ch_roulette.Legends[0].Enabled = false;
+            DataPoint dp = new DataPoint(100, 100);
+            ch_roulette.Series[legend1].Points.Add(dp);
+
+            //グラフのタイトル設定
+            string titles1 = "Title1";
+            ch_roulette.Titles.Add(titles1);
+            ch_roulette.Titles[titles1].Text = "";
+
+            //waveファイルを予め読み込む
+            player = new SoundPlayer(ROULETTE_SE_PATH);
+            
+            //グラフの矢印画像初期化
+            pi_arrow.Parent = ch_roulette;
+            pi_arrow.BackColor = Color.Transparent;
+            pi_arrow.ImageLocation = YAZIRUSI_PATH;
+
+            // オリジナルBitmapの取得
+            Bitmap bmp = new Bitmap(YAZIRUSI_PATH);
+            org = new Bitmap(bmp, YAZIRUSI_PIC_X, YAZIRUSI_PIC_Y);
+            bmp.Dispose();
+
+            // オリジナルBitmapをpictureBoxに設定
+            pi_arrow.Image = org;
+
+            //項目数に初期値を設定
+            num_rottime.Text = "1";
+
+            //タイマーの設定
+            timer = new System.Timers.Timer(ONE_SEC / MAX_FRAME);
+            // タイマーの処理
+            timer.Elapsed += (t_sender, t_e) =>
+            {
+                //回転角度計算
+                if (ang > MAX_ANGLE)
+                {
+                    ang = ang - MAX_ANGLE;
+                }
+                ang += (( MAX_ANGLE / rot_time ) / MAX_FRAME) * 2;
+
+                // ビットマップ(Bitmap)を回転する
+                pi_arrow.Image = RotateBitmap(org, ang, YAZIRUSI_PIC_X / 2, YAZIRUSI_PIC_Y / 2);
+
+                //ストップボタンを押してからの処理
+                if (!rotationflg)
+                {
+                    //ストップボタンを押してから一定時間経過後に回転を止める
+                    if (stopcount < stoptime )
+                    {
+                        stopcount = stopcount + 1;
+                    }
+                    else
+                    {
+                        Invoke(new SetFocusDelegate(ControlEnable));
+                        timer.Stop();
+                    }
+                }
+            };
+
+            ItemCountChange();
+
+        }
+
+        private void num_itemcount_ValueChanged(object sender, EventArgs e)
+        {
+            ItemCountChange();
+        }
+
+        public void ItemCountChange()
+        {
+            int inputcount = (int)num_itemcount.Value;
+            //if(int.TryParse(num_itemcount.Value, out inputcount))
+            {
+                //項目数に変化があった時のみ処理
+                if (inputcount > 0 && inputcount != datacount)
+                {
+                    //グラフリセット
+                    ch_roulette.Series.Clear();
+                    string legend1 = "Graph1";
+                    ch_roulette.Series.Add(legend1);
+                    ch_roulette.Series[legend1].ChartType = SeriesChartType.Pie;
+                    ch_roulette.Series[legend1].BorderColor = Color.Black;
+                    ch_roulette.Legends[0].Enabled = false;
+                    DataPoint dp = new DataPoint(100, 100);
+                    ch_roulette.Series[legend1].Points.Add(dp);
+
+                    //グラフ名と割合入力欄の削除
+                    //グラフ名
+                    for (int i = 0; i < datacount; i++)
+                    {
+                        Control[] namelist = this.pa_item.Controls.Find("tb_itemname" + i.ToString(), true);
+
+                        if (namelist.Length == 0)
+                            continue;
+
+                        this.pa_item.Controls.Remove(namelist[0]);
+                    }
+
+                    //割合
+                    for (int i = 0; i < datacount; i++)
+                    {
+                        Control[] numlist = this.pa_item.Controls.Find("num_ratio" + i.ToString(), true);
+
+                        if (numlist.Length == 0)
+                            continue;
+
+                        this.pa_item.Controls.Remove(numlist[0]);
+                    }
+
+                    //グラフ名と割合入力欄生成
+                    NumericUpDown[] num_num = new NumericUpDown[inputcount];
+                    TextBox[] tb_name = new TextBox[inputcount];
+                    for (int i = 0; i < inputcount; i++)
+                    {
+                        //グラフ名
+                        tb_name[i] = new TextBox();
+                        tb_name[i].Location = new Point(0, 5 + (31 * i));
+                        tb_name[i].Size = new Size(55, 31);
+                        tb_name[i].Name = "tb_itemname" + i.ToString();
+                        tb_name[i].Leave += new EventHandler(text_Leave);
+                        tb_name[i].TextAlign = HorizontalAlignment.Right;
+                        tb_name[i].MaxLength = 20;
+                        this.pa_item.Controls.Add(tb_name[i]);
+
+                        //割合
+                        num_num[i] = new NumericUpDown();
+                        num_num[i].Location = new Point(65, 5 + (31 * i));
+                        num_num[i].Size = new Size(55, 31);
+                        num_num[i].Name = "num_ratio" + i.ToString();
+                        num_num[i].ValueChanged += new EventHandler(text_Leave);
+                        num_num[i].TextAlign = HorizontalAlignment.Right;
+                        num_num[i].Maximum = 50;
+                        num_num[i].Minimum = 1;
+                        this.pa_item.Controls.Add(num_num[i]);
+                    }
+                    datacount = inputcount;
+
+                    GraphChange();
+                }
+            }
+        }
+
+        private void text_Leave(object sender, EventArgs e)
+        {
+            GraphChange();
+        }
+
+        public void GraphChange()
+        {
+            int[] values = new int[0];
+            string[] names = new string[0];
+
+            //入力内容取得
+            for (int i = 0; i < datacount; i++)
+            {
+                Control[] numlist = this.pa_item.Controls.Find("num_ratio" + i.ToString(), true);
+                Control[] namelist = this.pa_item.Controls.Find("tb_itemname" + i.ToString(), true);
+
+                if (numlist.Length == 0)
+                    continue;
+
+                NumericUpDown num = (NumericUpDown)this.pa_item.Controls.Find("num_ratio" + i.ToString(), true)[0];
+
+                int inputcount = (int)num.Value;
+                //if (int.TryParse(numlist[0].Text, out inputcount))
+                {
+                    Array.Resize(ref values, values.Length + 1);
+                    values[values.Length - 1] = inputcount;
+
+                    Array.Resize(ref names, names.Length + 1);
+                    if (namelist.Length == 0)
+                    {
+                        names[names.Length - 1] = "";
+                    }
+                    else
+                    {
+                        names[names.Length - 1] = namelist[0].Text;
+                    }
+
+                }
+            }
+
+            if (values.Length != 0)
+            {
+                //グラフに入力内容を反映
+                ch_roulette.Series.Clear();
+                string legend1 = "Graph1";
+                ch_roulette.Series.Add(legend1);
+                ch_roulette.Series[legend1].ChartType = SeriesChartType.Pie;
+                ch_roulette.Series[legend1].BorderColor = Color.Black;
+                ch_roulette.Legends[0].Enabled = false;
+                // 各項目の値を加算して合計(全体の大きさ)を算出
+                double total = 0.0;
+                foreach (double d in values)
+                {
+                    total += d;
+                }
+                // データをシリーズにセット
+                for (int i = 0; i < values.Length; i++)
+                {
+                    double rate = (values[i] / total) * 100.0;  // 割合を算出
+                    DataPoint dp = new DataPoint(rate, rate);
+                    dp.Label = names[i];
+                    ch_roulette.Series[legend1].Points.Add(dp);
+                }
+            }
+        }
+
+        private void bu_start_Click(object sender, EventArgs e)
+        {
+            if (rotationflg)
+            {
+                rotationflg = false;
+                stopcount = 0;
+                Random random = new Random();
+                stoptime = random.Next(STOP_EXTENSION_MIN_TIME, STOP_EXTENSION_MAX_TIME);
+                bu_start.Enabled = false;
+            }
+            else
+            {
+                // タイマーを開始する
+                timer.Start();
+                rotationflg = true;
+                bu_start.Text = "Stop!"; //ボタンのテキスト変更
+                //stopするまで項目を入力できないようにする
+                tb_title.Enabled = false;
+                num_itemcount.Enabled = false;
+                num_rottime.Enabled = false;
+
+                for (int i = 0; i < datacount; i++)
+                {
+                    Control[] numlist = this.pa_item.Controls.Find("num_ratio" + i.ToString(), true);
+                    Control[] namelist = this.pa_item.Controls.Find("tb_itemname" + i.ToString(), true);
+
+                    if (numlist.Length == 0)
+                        continue;
+
+                    numlist[0].Enabled = false;
+                    namelist[0].Enabled = false;
+                }
+
+                //ルーレット音開始
+                PlaySound();
+            }
+        }
+
+        private void tb_title_Leave(object sender, EventArgs e)
+        {
+            //グラフのタイトル表示
+            ch_roulette.Titles[0].Text = tb_title.Text;
+        }
+
+        private void num_rottime_ValueChanged(object sender, EventArgs e)
+        {
+            //回転速度変更
+            float inputcount = (float)num_rottime.Value;
+            //if (float.TryParse(num_rottime.Text, out inputcount))
+            {
+                if (inputcount >= 0.1)
+                {
+                    rot_time = inputcount;
+                }
+                else
+                {
+                    num_rottime.Text = "0.1";
+                    rot_time = 0.1f;
+                }
+            }
+        }
+
+        private void Form_Click(object sender, EventArgs e)
+        {
+            //フォーカスを外す
+            ActiveControl = null;
+        }
+
+        delegate void SetFocusDelegate();
+
+        public void ControlEnable()
+        {
+            bu_start.Enabled = true;
+            bu_start.Text = "Start!"; //ボタンのテキスト変更
+
+            //項目を入力できるようにする
+            tb_title.Enabled = true;
+            num_itemcount.Enabled = true;
+            num_rottime.Enabled = true;
+
+            for (int i = 0; i < datacount; i++)
+            {
+                Control[] numlist = this.pa_item.Controls.Find("num_ratio" + i.ToString(), true);
+                Control[] namelist = this.pa_item.Controls.Find("tb_itemname" + i.ToString(), true);
+
+                if (numlist.Length == 0)
+                    continue;
+
+                numlist[0].Enabled = true;
+                namelist[0].Enabled = true;
+            }
+
+            StopSound();
+        }
+
+        /// ビットマップ(Bitmap)を回転する
+        public Bitmap RotateBitmap(Bitmap bmp, float angle, int x, int y)
+        {
+            Bitmap bmp2 = new Bitmap((int)bmp.Width, (int)bmp.Height);
+            Graphics g = Graphics.FromImage(bmp2);
+
+            g.TranslateTransform(-x, -y);
+            g.RotateTransform(angle, System.Drawing.Drawing2D.MatrixOrder.Append);
+            g.TranslateTransform(x, y, System.Drawing.Drawing2D.MatrixOrder.Append);
+
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+
+            g.DrawImageUnscaled(bmp, 0, 0);
+            g.Dispose();
+
+            return bmp2;
+        }
+
+        //WAVEファイルを再生する
+        private void PlaySound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+
+                //非同期再生する
+                //player.Play();
+
+                //ループ再生する
+                player.PlayLooping();
+
+                //最後まで再生し終えるまで待機する
+                //player.PlaySync();
+            }
+        }
+
+        //再生されている音を止める
+        private void StopSound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+            }
+        }
+    }
+}
